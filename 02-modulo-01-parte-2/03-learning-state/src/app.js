a40eb60ab88367d99c0415db3270cb0946c964e9
FELIPE MARTINS ABREU <felipe.abreu@trt1.jus.br>
2023-02-02 10:21:46 -0300

    exemplo de setstate

--- 02-modulo-01-parte-2/03-learning-state/src/app.js
+++ 02-modulo-01-parte-2/03-learning-state/src/app.js
@@ -1,18 +1,42 @@
-'use strict'
+"use strict";
 
-import React, { Component } from 'react'
-import Button from './button'
+import React, { Component } from "react";
+import Button from "./button";
+
+const NOME = "Felipe";
+let contador = 0;
 
 class App extends Component {
-  render () {
+  constructor() {
+    super();
+    this.state = {
+      text: NOME,
+    };
+  }
+
+  render() {
     return (
       <div>
-        <Button>
-          Clique em mim
-        </Button>
+        <div
+          className="container"
+          onClick={() =>
+            this.setState({
+              text:
+                NOME +
+                " - Você clicou aqui " +
+                ++contador +
+                (contador == 1 ? " vez!" : " vezes!"),
+            })
+          }
+        >
+          {this.state.text}
+        </div>
+        <div>
+          <Button handleClick={() => {}}>Clique em mim</Button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
